f884c39c0afa833402fc4919643a838b906a7abe
Leong PH <leong@mail.com>
2021-02-01 10:59:29 +0800

    Initial Commit

---
+++ c_assignment_2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_assignment_2
+//
+//  Created by Leong Ping How on 01/02/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
